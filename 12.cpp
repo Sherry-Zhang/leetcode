f28d38650f049751ddcafcc9d6da4d3118957e8d
zhangshu <9940706@163.com>
2017-01-22 19:19:04 +0800

    20170122

---
+++ 12.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//beats 10.61%
+class Solution {
+public:
+    string intToRoman(int num) {
+        char R[2][4] = {
+            { 'I', 'X', 'C', 'M' },		//1,10,100,1000
+            { 'V', 'L', 'D' }				//5,50,500
+        };
+        int c[4];
+        for (int i = 0; i < 4; ++i)
+        {
+            c[i] = num % 10;		//依次存放个/十/百/千位的系数
+            num /= 10;
+        }
+        string ans = "";
+        for (int i = 3; i >= 0; --i)
+        {
+            if (c[i] < 4)
+                ans.append(c[i], R[0][i]);
+            else if (c[i] == 4)
+                ans = ans + R[0][i] + R[1][i];	//右边减左边
+            else if (c[i] == 5)
+                ans += R[1][i];
+            else if (c[i] > 5 && c[i] < 9)
+            {
+                ans += R[1][i];
+                ans.append(c[i] - 5, R[0][i]);
+            }
+            else if (c[i] == 9)
+                ans += R[0][i] + R[0][i + 1];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    int num;
+    Solution ans;
+    cin >> num;
+    cout << ans.intToRoman(num) << endl;
+    return 0;
+}
---
+++ 13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+//问题：将0-3999范围内的罗马数字转换成整数表示
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> m = {
+            { 'I', 1 }, { 'X', 10 }, { 'C', 100 }, { 'M', 1000 },
+            { 'V', 5 }, { 'L', 50 }, { 'D', 500 }
+        };
+        int len = s.size();
+        int num = 0;    //转换后的数字
+        int tmp = m[s[0]];
+        int cmp;
+        for (auto &c : s)       //对字符串的每个字符进行遍历
+        {
+            cmp = m[c] / tmp;
+            if (cmp > 1 && cmp <= 10)      //如果相邻字符右边代表的值比左边的大，且不超过1个数量级，就相减
+                num = num - tmp + m[c] - tmp;        //需要多减一次上次循环加上的tmp
+            else
+                num += m[c];
+            tmp = m[c];
+        }
+        return num;
+    }
+};
+
+int main()
+{
+    string s;
+    Solution ans;
+    cin >> s;
+    cout << ans.romanToInt(s) << endl;
+    return 0;
+}
